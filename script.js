6d6158ee40680e273f03e8d34f05c627096c7667
Arpita Jauhari <120579974+arpitajauhari@users.noreply.github.com>
2024-06-17 12:35:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+// form repeater
+$(document).ready(function(){
+    $('.repeater').repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': ''
+        },
+        show:function(){
+            $(this).slideDown();
+        },
+        hide: function(deleteElement){
+            $(this).slideUp(deleteElement);
+            setTimeout(() => {
+                generateCV();
+            }, 500);
+        },
+        isFirstItemUndeletable: true
+    })
+})
+ 104 changes: 104 additions & 0 deletions104  
+index.html
